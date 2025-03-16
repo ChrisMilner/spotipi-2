6024b7ced1d328cf5e5d9f66d236dc1e55cababa
Chris Milner <christopherdmilner@gmail.com>
2025-03-16 01:04:56 +0000

    Use original spotipi approach for service

--- install-service.sh
+++ install-service.sh
@@ -8,7 +8,7 @@ systemctl daemon-reload
 echo "Creating spotipi2 service..."
 cp service/spotipi2.service /etc/systemd/system/
 install_path=$(pwd)
-sed -i -e "s/install_path/$install_path/g" /etc/systemd/system/spotipi2.service
+sudo sed -i -e "/\[Service\]/a ExecStart=python3 ${install_path}/src/spotipi.py &" /etc/systemd/system/spotipi2.service
 systemctl daemon-reload
 systemctl start spotipi2
 systemctl enable spotipi2
