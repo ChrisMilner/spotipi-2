0628558ef3681a4614211bcd74ab39d0b1410047
Chris Milner <christopherdmilner@gmail.com>
2025-03-16 00:16:28 +0000

    Make sure we can execute the install script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+set -e
+
 echo "Installing Python dependencies..."
 python3 -m pip install -r requirements.txt
 
 echo "Downloading rgb-matrix software setup..."
 curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh > rgb-matrix.sh
+chmod u+x rgb-matrix.sh
 
 echo "Running rgb-matrix software setup..."
 sudo ./rgb-matrix.sh
