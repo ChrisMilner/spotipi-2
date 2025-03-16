3f82e3c98fe06004010676ad6bb6661ac883225e
Chris Milner <christopherdmilner@gmail.com>
2025-03-16 01:01:59 +0000

    Fix sed quotes

--- install-service.sh
+++ install-service.sh
@@ -8,7 +8,7 @@ systemctl daemon-reload
 echo "Creating spotipi2 service..."
 cp service/spotipi2.service /etc/systemd/system/
 install_path=$(pwd)
-sed -i -e 's/install_path/$install_path/g' /etc/systemd/system/spotipi2.service
+sed -i -e "s/install_path/$install_path/g" /etc/systemd/system/spotipi2.service
 systemctl daemon-reload
 systemctl start spotipi2
 systemctl enable spotipi2
