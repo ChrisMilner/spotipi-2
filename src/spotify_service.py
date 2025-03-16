64809e8bd4fab7475972978a4919eb3ce70cc5a1
Chris Milner <christopherdmilner@gmail.com>
2025-03-16 00:42:26 +0000

    Change cache filename

--- src/spotify_service.py
+++ src/spotify_service.py
@@ -15,7 +15,7 @@ class SpotifyService:
             client_id=config["SPOTIPY"]["ClientId"],
             client_secret=config["SPOTIPY"]["ClientSecret"],
             redirect_uri=config["SPOTIPY"]["RedirectURI"],
-            cache_path='~/.cache',
+            cache_path='~/cache.txt',
             open_browser=False
         )
 
